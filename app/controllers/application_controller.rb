2e7f07a140604ccc1b8dcb010fa3b168e3df59e0
fedemarco <fedemarco@gmail.com>
2013-06-23 16:40:24 -0300

    Product and simple layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :show_time
+  
+  def show_time
+    @time = Time.now.to_formatted_s(:short)
+  end
 end
