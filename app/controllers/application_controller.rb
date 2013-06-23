012e1f338d016b86203c760488d2ffcb44a9f3b1
fedemarco <fedemarco@gmail.com>
2013-06-23 18:28:35 -0300

    Shopping Cart as a table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::Base
   
   before_filter :show_time
   
+  private
+  
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+  
   def show_time
     @time = Time.now.to_formatted_s(:short)
   end
+  
 end
