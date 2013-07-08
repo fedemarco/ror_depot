d33f9a0cd1e96982169eaed5e33c7c03708fad8a
fedemarco <fedemarco@gmail.com>
2013-07-08 18:08:51 -0300

    Updated files

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,7 @@
 class LineItem < ActiveRecord::Base
   attr_accessible :product, :product_id, :id, :updated_at, :cart_id, :created_at, :quantity
   
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
   
